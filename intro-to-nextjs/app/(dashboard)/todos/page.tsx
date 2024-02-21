4bd0344ab4b139139676bf0988ebe1201d46ab92
Welcome ! <usmanubaid9@gmail.com>
2024-02-21 17:23:16 +0100

    fetching data with prisma

--- intro-to-nextjs/app/(dashboard)/todos/page.tsx
+++ intro-to-nextjs/app/(dashboard)/todos/page.tsx
@@ -1,5 +1,18 @@
-const TodosPage = () => {
-  return <div>TodosPage</div>
-}
+import TodoList from "@/components/TodoList";
+import db from "@/utils/db";
 
-export default TodosPage
+const getData = async () => {
+  const todos = await db.todo.findMany({});
+  return todos;
+};
+
+const TodosPage = async () => {
+  const todos = await getData();
+  return (
+    <div>
+      <TodoList todos={todos} />
+    </div>
+  );
+};
+
+export default TodosPage;
